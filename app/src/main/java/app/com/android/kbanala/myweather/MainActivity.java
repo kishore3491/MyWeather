795b5fb9775de3e25bafc16f8d622d929116947e
kishore3491 <kishore.meetme@gmail.com>
2015-06-15 01:04:59 -0500

    Activity for Settings

--- app/src/main/java/app/com/android/kbanala/myweather/MainActivity.java
+++ app/src/main/java/app/com/android/kbanala/myweather/MainActivity.java
@@ -1,5 +1,6 @@
 package app.com.android.kbanala.myweather;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -31,6 +32,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
